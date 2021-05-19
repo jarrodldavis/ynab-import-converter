05e60570851b08f4815793e1b759f12bfbf6b468
Jarrod Davis <developer@jarrodldavis.com>
2021-05-19 04:27:56 +0000

    Add rudimentary support for Betterment Market Changes

--- src/sources/betterment/market_changes.rs
+++ src/sources/betterment/market_changes.rs
@@ -1,6 +1,47 @@
 use std::path::PathBuf;
 
+use chrono::NaiveDate;
 use clap::Clap;
+use csv::{Reader, Writer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+struct InputRecord {
+    #[serde(rename = "Date")]
+    date: NaiveDate,
+
+    #[serde(rename = "Market Change Amount")]
+    amount: f32,
+}
+
+#[derive(Debug, Serialize)]
+struct OutputRecord {
+    #[serde(rename = "Date")]
+    date: NaiveDate,
+
+    #[serde(rename = "Payee")]
+    payee: &'static str,
+
+    #[serde(rename = "Memo")]
+    memo: String,
+
+    #[serde(rename = "Amount")]
+    amount: f32,
+}
+
+impl From<&InputRecord> for OutputRecord {
+    fn from(input_record: &InputRecord) -> Self {
+        Self {
+            date: input_record.date,
+            payee: "Betterment",
+            memo: format!(
+                "Market Changes for {}",
+                input_record.date.format("%A, %-d %B, %C%y")
+            ),
+            amount: input_record.amount,
+        }
+    }
+}
 
 #[derive(Clap)]
 #[clap(author)]
@@ -30,5 +71,15 @@ impl Opts {
         println!("Betterment -> Investment -> Market Changes");
         println!("input: {:?}", self.input_file);
         println!("output: {:?}", self.output_file);
+
+        let mut reader = Reader::from_path(&self.input_file).expect("csv reader");
+        let mut writer = Writer::from_path(&self.output_file).expect("csv writer");
+
+        for result in reader.deserialize() {
+            let input_record: InputRecord = result.expect("csv input record");
+            let output_record = OutputRecord::from(&input_record);
+            println!("{:?} -> {:?}", input_record, output_record);
+            writer.serialize(output_record).expect("csv output record");
+        }
     }
 }
